37506dfd051bbd6588731e8196ab2be3ac65d33d
GUILHERME NINOV DE MEIRA <guininovmeira@hotmail.com>
2020-11-30 10:27:54 -0300

    noov

---
+++ site 03/sobre.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./reset.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Suitless - Sobre</title>
+</head>
+<body>
+    <div class="navbar">
+        <a href="/site%2003/"><img src="logo.png" alt=""></a>
+    </div>
+    <div class="title">
+        <h1>Crie uma conta!</h1>
+        <p>Para criar uma conta, <a href="https://suitless.com.br/register" target="_blank">clique nesse link!</a><br>Ou se quiser saber mais sobre essa rede social,<a href="https://suitless.com.br/sobre" target="_blank"> clique aqui</a></p>
+    </div>
+    
+</body>
+</html>
+
